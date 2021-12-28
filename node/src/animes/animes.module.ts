195c379d13f402273956f2c7d024641f337e861a
giovanifranz <giovanifranz151@gmail.com>
2021-12-28 00:21:22 -0300

    first commit

--- node/src/animes/animes.module.ts
+++ node/src/animes/animes.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HttpModule } from '@nestjs/axios';
+import { AnimesService } from './services/animes.service';
+import { DatabaseService, ExternalApiService, SonicService } from './services'
 import { AnimesController } from './animes.controller';
 import { SonicModule } from 'src/sonic/sonic.module';
 import { AnimeSchema } from './entities/anime.entity';
-import { AnimesService, DatabaseService, ExternalApiService, SonicService } from './services'
+
 
 @Module({
   imports: [
