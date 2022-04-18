01a215c5a95ec2e8da160a74b978cc27910800d6
giovanifranz <giovanifranz@gmail.com>
2022-04-18 03:54:08 -0300

    refactor: :recycle: melhorias no código

--- src/http/graphql/resolvers/anime.resolver.ts
+++ src/http/graphql/resolvers/anime.resolver.ts
@@ -12,17 +12,17 @@ export class AnimeResolver {
   constructor(private readonly animeService: AnimeService, private readonly quoteService: QuoteService) {}
 
   @Query(() => Anime)
-  async searchAnimeById(@Args('id') id: number) {
-    return await this.animeService.searchAnimeById(id)
+  searchAnimeById(@Args('id') id: number) {
+    return this.animeService.searchAnimeById(id)
   }
 
   @Query(() => [Anime])
-  async searchAnimesByTitle(@Args('title') title: string) {
-    return await this.animeService.searchAnimesByTitle(formatText(title))
+  searchAnimesByTitle(@Args('title') title: string) {
+    return this.animeService.searchAnimesByTitle(formatText(title))
   }
 
   @ResolveField()
-  async quotes(@Parent() anime: Anime) {
-    return await this.quoteService.getAnimesQuoteByTitle(formatTitleForQuote(anime.title))
+  quotes(@Parent() anime: Anime) {
+    return this.quoteService.getAnimesQuoteByTitle(formatTitleForQuote(anime.title))
   }
 }
